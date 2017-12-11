ce8cb0d55a54020c58d478919996e7235bb22d73
Jay Kim <jkim208@masonlive.gmu.edu>
2017-12-11 08:50:16 -0500

    Fixed function draw()

--- test.js
+++ test.js
@@ -1,15 +1,62 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
+// Create unit
+const box = 32;
 
 // Load images
-let imageName = new Image();
-imageName.src = "path/img.png";
-let audioName = new Audio();
-audioName.src = "path/audio.png";
-audioName.ply();
+const ground = new Image();
+ground.src = "img/ground.png"
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// Create snake
+let snake = [];
+snake[0] = {
+	x : 9 * box,
+	y : 10 * box
+}
+
+// Create food
+let food = {
+	x : Math.floor(Math.random() * 17+1) * box,
+	y : Math.floor(Math.random() * 15+3) * box
+}
+
+// Create score
+let score = 0;
+
+// Control snake
 
 
-// Draw images
-ctx.drawImage(imageName, X, Y, Width, Height);
-ctx.draw
+
+// Draw
+function draw(){
+	ctx.drawImage(ground,0,0);
+	for(let i=0; i<snake.length; i++){
+		ctx.fillStyle=(i==0)?"green" : "white";
+		ctx.fillRect(snake[i].x,snake[i].y,box,box);
+
+		ctx.strokeStyle="red";
+		ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+	}
+
+	ctx.drawImage(foodImg, food.x, food.y);
+
+	ctx.fillStyle = "white";
+	ctx.font = "45px Changa one";
+	ctx.fillText(score,2*box,1.6*box);
+}
+
+// Call draw function every 100ms
+let game = setInterval(draw,100);
+
+
+
+
+
+
+
+
+
