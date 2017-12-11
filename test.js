995edb13b52de2086332137a7db5eab6bae6c8e3
Jay Kim <jkim208@masonlive.gmu.edu>
2017-12-11 09:01:12 -0500

    Added collision function

--- test.js
+++ test.js
@@ -27,21 +27,32 @@ let food = {
 // Create score
 let score = 0;
 
+
 // Control snake
 let d;
 
 document.addEventListener("keydown",direction);
 
 function direction(event){
-	if(event.keyCode == 37){
+	if(event.keyCode == 37 && d != "RIGHT"){
 		d = "LEFT";
-	}else if(event.keyCode == 38){
+	}else if(event.keyCode == 38 && d != "DOWN"){
 		d = "UP";
-	}else if(event.keyCode == 39){
+	}else if(event.keyCode == 39 && d != "LEFT"){
 		d = "RIGHT";
-	}else if(event.keyCode == 40){
+	}else if(event.keyCode == 40 && d != "UP"){
 		d = "DOWN";
 	}
+}
+
+// Check collision function
+function collision(head, array){
+	for(let i=0; i<array.length; i++){
+		if(head.x == array[i].x && head.y == array[i].y){
+			return true;
+		}
+	}
+	return false;
 }
 
 
@@ -57,6 +68,43 @@ function draw(){
 	}
 
 	ctx.drawImage(foodImg, food.x, food.y);
+
+	// Old head position
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	// which direction
+	if (d=="LEFT") snakeX -=box;
+	if (d=="UP") snakeY -=box;
+	if (d=="RIGHT") snakeX +=box;
+	if (d=="DOWN") snakeX +=box;
+
+	// Snake eat food
+	if(snakeX == food.x && snakeY == snakeY){
+		score++;
+		// Generate new fod
+		food = {
+			x : Math.floor(Math.random()*17+1) * box,
+			y : Math.floor(Math.random()*15+3) * box
+		}
+		// Don't remove tail
+	}else {
+		// Remove tail
+		snake.pop();
+	}
+
+	// Add new head
+	let newHead = {
+		x : snakeX,
+		y : snakeY
+	}
+
+	// Game ends
+	if(snakeX < box || snakeX > 17*box || snakeY < 3*box || snakeY > 17*box || collision(newHead, snake)){
+		clearInterval(game);
+	}
+
+	snake.unshift(newHead);
 
 	ctx.fillStyle = "white";
 	ctx.font = "45px Changa one";
